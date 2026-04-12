50f66ef6a27164ebd6b2de276cd977b0394e1db5
Tri Wulandari <triwulandari3456@gmail.com>
2026-04-12 19:45:07 +0700

    update koneksi db

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,16 +1,23 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const env = process.env.NODE_ENV ;
 
-const sequelize = new Sequelize({
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres',
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,{
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: 
+      process.env.NODE_ENV === "development" 
+      ?(...msg) => console.log(msg) 
+      : false,
+    dialectOptions : {
+      requestTimeout : 3000,
+      encrypt: true
+    }  
+  }
+);
 
 module.exports = { sequelize };
